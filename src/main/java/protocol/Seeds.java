73ca57b594b30724452daa448bb8080a9cac513f
zimkamartin <martin.zimka@gmail.com>
2025-08-23 13:40:36 +0200

    ADD: Seeds.

---
+++ src/main/java/protocol/Seeds.java
@@ -0,0 +1,11 @@
+package protocol;
+
+class Seeds {
+    final byte[] seed1;
+    final byte[] seed2;
+
+    Seeds(byte[] seed1, byte[] seed2) {
+        this.seed1 = seed1;
+        this.seed2 = seed2;
+    }
+}
